0a135a7974096ec6bd1f8cd8331f737af6cccc70
ap94sd24 <adampan94@gmail.com>
2016-02-01 21:08:35 -0800

    Update compile.tcl

--- Lab 2 - Part 1/ucsdcse141l-labs/lab2/simple_core/src/tb/compile.tcl
+++ Lab 2 - Part 1/ucsdcse141l-labs/lab2/simple_core/src/tb/compile.tcl
@@ -1,1 +1,17 @@
 # TODO: Fill this out.
+#Edited by Adam
+if {[file isdirectory work]} {
+	vdel -all -lib work
+}
+
+#create library
+vlib work 
+
+#compile .sv files
+vlog -work work "../simple_definitions.sv"
+vlog -work work "../simple_alu.sv"
+vlog -work work "../simple_core.sv"
+vlog -work work "../simple_imem.sv"
+vlog -work work "../simple_reg_file.sv"
+vlog -work work "simple_core_tb.sv"
+
