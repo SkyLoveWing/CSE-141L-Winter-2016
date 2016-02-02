2c402c2f0c8d7b52d7624a46d37c66cb2cb91323
ap94sd24 <adampan94@gmail.com>
2016-02-01 21:13:01 -0800

    Update sim.tcl

--- Lab 2 - Part 1/ucsdcse141l-labs/lab2/simple_core/src/tb/sim.tcl
+++ Lab 2 - Part 1/ucsdcse141l-labs/lab2/simple_core/src/tb/sim.tcl
@@ -4,8 +4,8 @@
 vsim work.simple_core_tb
 
 #				Group Name      	        Radix 				  Signal(s)
-add wave    -noupdate	-group {simple_reg_file}     		-radix hexadecimal    /simple_core_tb/dut/rf/*
-add wave    -noupdate   -group {simple_alu}               	-radix hexadecimal    /simple_core_tb/dut/alu/rd_i
+add wave    -noupdate	  -group {simple_reg_file}     		-radix hexadecimal    /simple_core_tb/dut/rf/*
+add wave    -noupdate     -group {simple_alu}               	-radix hexadecimal    /simple_core_tb/dut/alu/rd_i
 add wave    -noupdate           -group {simple_alu}              	-radix hexadecimal    /simple_core_tb/dut/alu/rs_i
 add wave    -noupdate           -group {simple_alu}              	-radix hexadecimal    /simple_core_tb/dut/alu/op_i
 add wave    -noupdate           -group {simple_alu}               	-radix hexadecimal    /simple_core_tb/dut/alu/result_o
